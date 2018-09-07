58d7ab5ab668dc7f069c917c751ae6f1cea5209d
Dan Hintz <dhintz@live.com>
2018-09-07 02:08:53 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -12,4 +12,12 @@ class Genre
     song = Song.new(name, artist, self)
   end
   
+  def songs
+    Songs.all.select {|song| song.genre == self}
+  end
   
+  def artists
+    self.songs.collect {|song| song.artist}
+  end
+  
+end
