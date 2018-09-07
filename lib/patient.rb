bb59e6cd2ae5c43e997a59339292f6964582d5a0
Dan Hintz <dhintz@live.com>
2018-09-07 02:20:05 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -8,4 +8,16 @@ class Patient
     @@all << self
   end
   
-  def new_appointment(j)
+  def new_appointment(date, doctor)
+    appointment = Appointment.new(date, doctor, self)
+  end
+  
+  def appointments
+    Appointments.all.select {|appt| appt.patient == self}
+  end
+  
+  def doctors
+    self.appointments.collect {|appt| appt.doctor}
+  end
+  
+end
